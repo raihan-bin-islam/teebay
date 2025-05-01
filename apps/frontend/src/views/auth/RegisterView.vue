fd048941707b18a5a1dcd2d06997309f79efa7a1
raihan <rishaon0@gmail.com>
2025-05-01 16:11:28 +0600

    feat: add redirection links to auth forms

--- apps/frontend/src/views/auth/RegisterView.vue
+++ apps/frontend/src/views/auth/RegisterView.vue
@@ -6,9 +6,17 @@ import { Card, CardContent, CardHeader } from '@/components/ui/card'
 <template>
   <div class="size-full flex items-center justify-center">
     <Card class="min-w-xl max-w-xl">
-      <CardHeader class="text-center text-2xl font-bold">REGISTER</CardHeader>
+      <CardHeader class="text-center text-2xl font-bold">SIGN UP</CardHeader>
       <CardContent>
         <RegistrationForm />
+        <div class="mt-4 mx-auto w-fit">
+          <span>
+            Already have an account?
+            <RouterLink to="/login">
+              <a class="text-blue-500 hover:underline">Sign In</a>
+            </RouterLink>
+          </span>
+        </div>
       </CardContent>
     </Card>
   </div>
