e39c497f71ecb3a9bfe5fe1aaf2ca5d78c76d105
raihan <rishaon0@gmail.com>
2025-05-01 16:09:56 +0600

    feat: add product listing page

---
+++ apps/frontend/src/views/product/AllProducts.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import ProductCard from '@/components/cards/ProductCard.vue'
+import { useProducts } from '@/composables/useProducts'
+import { onMounted } from 'vue'
+import { RouterLink } from 'vue-router'
+
+const { fetchAllProducts, allProducts } = useProducts()
+onMounted(async () => await fetchAllProducts())
+</script>
+<template>
+  <main class="py-4">
+    <h2 class="text-2xl font-bold">All Products</h2>
+    <div class="space-y-3 py-4">
+      <div v-for="product in allProducts" :key="product.id" class="w-full relative">
+        <RouterLink class="z-50 absolute inset-0 w-full" :to="`/product/${product.id}`" />
+        <ProductCard :product="product" hideEdit />
+      </div>
+    </div>
+  </main>
+</template>
