07ba56c81b5853f15f66ed47a215f9f4f0b82499
raihan <rishaon0@gmail.com>
2025-05-01 16:07:21 +0600

    feat: add homepage & create/edit product flow

---
+++ apps/frontend/src/views/product/CreateProduct.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import CreateProductForm from '@/components/products/CreateProductForm.vue'
+</script>
+<template>
+  <main>
+    <CreateProductForm />
+  </main>
+</template>
