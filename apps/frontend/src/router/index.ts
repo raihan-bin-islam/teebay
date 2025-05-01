75921517114ab1b0aebc8eddbfbc0e10c46aa4c9
raihan <rishaon0@gmail.com>
2025-05-01 16:12:44 +0600

    feat: set up routing for all pages

--- apps/frontend/src/router/index.ts
+++ apps/frontend/src/router/index.ts
@@ -1,8 +1,15 @@
+import { tokenKey } from '@/data/config'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/product/MyProducts.vue'),
+      meta: { requiresAuth: true },
+    },
     {
       path: '/login',
       name: 'login',
@@ -15,7 +22,46 @@ const router = createRouter({
       component: () => import('../views/auth/RegisterView.vue'),
       meta: { requiresGuest: true },
     },
+    {
+      path: '/products',
+      name: 'products',
+      component: () => import('../views/product/AllProducts.vue'),
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/product/:id',
+      name: 'productById',
+      component: () => import('../views/product/SingleProduct.vue'),
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/transaction-history',
+      name: 'transactionHistory',
+      component: () => import('../views/history/TransactionHistory.vue'),
+      meta: { requiresAuth: true },
+    },
+
+    {
+      path: '/product/create',
+      name: 'productCreate',
+      component: () => import('../views/product/CreateProduct.vue'),
+      meta: { requiresAuth: true },
+    },
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = !!localStorage.getItem(tokenKey)
+
+  console.log({ isAuthenticated })
+
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    next({ name: 'login' })
+  } else if (to.meta.requiresGuest && isAuthenticated) {
+    next({ name: 'home' })
+  } else {
+    next()
+  }
 })
 
 export default router
