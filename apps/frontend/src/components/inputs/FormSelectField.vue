d8a7750796eca238426be71e416b480cb9c3e03c
raihan <rishaon0@gmail.com>
2025-05-01 16:04:53 +0600

    feat: custom inputs required to build create/edit/rental forms

---
+++ apps/frontend/src/components/inputs/FormSelectField.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { FormField, FormItem, FormLabel, FormControl, FormMessage } from '@/components/ui/form'
+import {
+  Select,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+  SelectValue,
+} from '@/components/ui/select'
+defineProps<{
+  name: string
+  label: string
+  placeholder?: string
+  options: { label: string; value: string }[]
+}>()
+</script>
+
+<template>
+  <FormField :key="name" :name="name" v-slot="{ componentField }">
+    <FormItem>
+      <FormLabel>{{ label }}</FormLabel>
+      <FormControl>
+        <Select v-bind="componentField">
+          <SelectTrigger class="w-full">
+            <SelectValue :placeholder="placeholder" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem v-for="opt in options" :key="opt.value" :value="opt.value">
+              {{ opt.label }}
+            </SelectItem>
+          </SelectContent>
+        </Select>
+      </FormControl>
+      <FormMessage />
+    </FormItem>
+  </FormField>
+</template>
---
+++ apps/frontend/src/components/inputs/FormTextareaField.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Textarea } from '@/components/ui/textarea'
+
+defineProps<{
+  name: string
+  label: string
+  placeholder?: string
+}>()
+</script>
+
+<template>
+  <FormField :name="name" v-slot="{ componentField }">
+    <FormItem>
+      <FormLabel>{{ label }}</FormLabel>
+      <FormControl>
+        <Textarea v-bind="componentField" :placeholder="placeholder" class="resize-none" />
+      </FormControl>
+      <FormMessage />
+    </FormItem>
+  </FormField>
+</template>
