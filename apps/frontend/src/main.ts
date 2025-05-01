9b90f6659054a3e2031d51b468790acf214520d4
raihan <rishaon0@gmail.com>
2025-05-01 16:18:26 +0600

    chore: clean up codebase

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import { DefaultApolloClient } from '@vue/apollo-composable'
 
 import App from './App.vue'
@@ -8,7 +7,6 @@ import apolloClient from './apollo'
 
 const app = createApp(App)
 
-app.use(createPinia())
 app.use(router)
 
 // Provide the Apollo client to the app
