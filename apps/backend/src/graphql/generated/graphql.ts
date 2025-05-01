c5656aaa3ef6d8a043b8d583c656dd5024d3058d
raihan <rishaon0@gmail.com>
2025-05-01 15:59:14 +0600

    feat: update codegen config

--- apps/backend/src/graphql/generated/graphql.ts
+++ apps/backend/src/graphql/generated/graphql.ts
@@ -1,4 +1,5 @@
 import { GraphQLResolveInfo } from 'graphql';
+import { User, Product, Category, Rental, Transaction, ProductStatus } from '@prisma/client';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
@@ -6,7 +7,9 @@ export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: 
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
 export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
 export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 export type RequireFields<T, K extends keyof T> = Omit<T, K> & { [P in K]-?: NonNullable<T[P]> };
+export type EnumResolverSignature<T, AllowedValues = any> = { [key in keyof T]?: AllowedValues };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: { input: string; output: string; }
@@ -22,10 +25,46 @@ export type AuthPayload = {
   user: User;
 };
 
+export type Category = {
+  __typename?: 'Category';
+  id: Scalars['Int']['output'];
+  name: Scalars['String']['output'];
+  products?: Maybe<Array<Product>>;
+};
+
+export enum CategoryName {
+  Electronics = 'ELECTRONICS',
+  Furniture = 'FURNITURE',
+  HomeAppliances = 'HOME_APPLIANCES',
+  Outdoor = 'OUTDOOR',
+  SportingGoods = 'SPORTING_GOODS',
+  Toys = 'TOYS'
+}
+
 export type Mutation = {
   __typename?: 'Mutation';
+  buyProduct: Transaction;
+  createProduct: Product;
+  deleteProduct: Product;
   login: AuthPayload;
   register: AuthPayload;
+  rentProduct: Rental;
+  updateProduct: Product;
+};
+
+
+export type MutationBuyProductArgs = {
+  productId: Scalars['Int']['input'];
+};
+
+
+export type MutationCreateProductArgs = {
+  data: ProductCreateInput;
+};
+
+
+export type MutationDeleteProductArgs = {
+  id: Scalars['Int']['input'];
 };
 
 
@@ -44,18 +83,152 @@ export type MutationRegisterArgs = {
   phone?: InputMaybe<Scalars['String']['input']>;
 };
 
+
+export type MutationRentProductArgs = {
+  data: RentalCreateInput;
+};
+
+
+export type MutationUpdateProductArgs = {
+  data: ProductUpdateInput;
+  id: Scalars['Int']['input'];
+};
+
+export type Product = {
+  __typename?: 'Product';
+  categories?: Maybe<Array<Maybe<Category>>>;
+  description: Scalars['String']['output'];
+  id: Scalars['Int']['output'];
+  owner: User;
+  ownerId: Scalars['Int']['output'];
+  price: Scalars['Float']['output'];
+  rentPeriod?: Maybe<Scalars['String']['output']>;
+  rentPrice?: Maybe<Scalars['Float']['output']>;
+  rentals?: Maybe<Array<Rental>>;
+  status: ProductStatus;
+  title: Scalars['String']['output'];
+  transactions?: Maybe<Array<Transaction>>;
+  views: Scalars['Int']['output'];
+};
+
+export type ProductCreateInput = {
+  categoryIds: Array<Scalars['Int']['input']>;
+  description: Scalars['String']['input'];
+  price: Scalars['Float']['input'];
+  rentPeriod?: InputMaybe<Scalars['String']['input']>;
+  rentPrice?: InputMaybe<Scalars['Float']['input']>;
+  title: Scalars['String']['input'];
+};
+
+export enum ProductStatus {
+  Available = 'AVAILABLE',
+  Rented = 'RENTED',
+  Sold = 'SOLD'
+}
+
+export type ProductUpdateInput = {
+  categoryIds?: InputMaybe<Array<Scalars['Int']['input']>>;
+  description?: InputMaybe<Scalars['String']['input']>;
+  price?: InputMaybe<Scalars['Float']['input']>;
+  rentPeriod?: InputMaybe<Scalars['String']['input']>;
+  rentPrice?: InputMaybe<Scalars['Float']['input']>;
+  title?: InputMaybe<Scalars['String']['input']>;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  categories: Array<Category>;
+  category?: Maybe<Category>;
+  myBorrowedProducts: Array<Rental>;
+  myBoughtProducts: Array<Transaction>;
+  myLentProducts: Array<Rental>;
+  myProduct?: Maybe<Product>;
+  myProducts: Array<Product>;
+  mySoldProducts: Array<Transaction>;
+  product?: Maybe<Product>;
+  products: Array<Product>;
+  rentals: Array<Rental>;
+  transactions: Array<Transaction>;
+  user?: Maybe<User>;
+  users: Array<User>;
+};
+
+
+export type QueryCategoryArgs = {
+  id: Scalars['Int']['input'];
+};
+
+
+export type QueryMyProductArgs = {
+  id: Scalars['Int']['input'];
+};
+
+
+export type QueryProductArgs = {
+  id: Scalars['Int']['input'];
+};
+
+
+export type QueryUserArgs = {
+  id: Scalars['Int']['input'];
+};
+
+export type Rental = {
+  __typename?: 'Rental';
+  createdAt: Scalars['String']['output'];
+  endDate: Scalars['String']['output'];
+  id: Scalars['Int']['output'];
+  owner: User;
+  ownerId: Scalars['Int']['output'];
+  product: Product;
+  productId: Scalars['Int']['output'];
+  renter: User;
+  renterId: Scalars['Int']['output'];
+  startDate: Scalars['String']['output'];
+};
+
+export type RentalCreateInput = {
+  endDate: Scalars['String']['input'];
+  productId: Scalars['Int']['input'];
+  startDate: Scalars['String']['input'];
+};
+
+export type Transaction = {
+  __typename?: 'Transaction';
+  buyer: User;
+  buyerId: Scalars['Int']['output'];
+  createdAt: Scalars['String']['output'];
+  id: Scalars['Int']['output'];
+  product: Product;
+  productId: Scalars['Int']['output'];
+  seller: User;
+  sellerId: Scalars['Int']['output'];
+};
+
 export type User = {
   __typename?: 'User';
   address?: Maybe<Scalars['String']['output']>;
+  /** @deprecated Use transactions instead */
+  boughtProducts?: Maybe<Array<Transaction>>;
   email: Scalars['String']['output'];
   firstName: Scalars['String']['output'];
   id: Scalars['Int']['output'];
   lastName: Scalars['String']['output'];
+  /** @deprecated Use rentals instead */
+  lentProducts?: Maybe<Array<Rental>>;
   password: Scalars['String']['output'];
   phone?: Maybe<Scalars['String']['output']>;
+  products?: Maybe<Array<Product>>;
+  rentals?: Maybe<Array<Rental>>;
+  /** @deprecated Use rentals instead */
+  rentedProducts?: Maybe<Array<Rental>>;
+  /** @deprecated Use transactions instead */
+  soldProducts?: Maybe<Array<Transaction>>;
+  transactions?: Maybe<Array<Transaction>>;
 };
 
-
+export type WithIndex<TObject> = TObject & Record<string, any>;
+export type ResolversObject<TObject> = WithIndex<TObject>;
 
 export type ResolverTypeWrapper<T> = Promise<T> | T;
 
@@ -125,50 +298,157 @@ export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs
 
 
 /** Mapping between all available schema types and the resolvers types */
-export type ResolversTypes = {
-  AuthPayload: ResolverTypeWrapper<AuthPayload>;
+export type ResolversTypes = ResolversObject<{
+  AuthPayload: ResolverTypeWrapper<Omit<AuthPayload, 'user'> & { user: ResolversTypes['User'] }>;
   Boolean: ResolverTypeWrapper<Scalars['Boolean']['output']>;
+  Category: ResolverTypeWrapper<Category>;
+  CategoryName: CategoryName;
+  Float: ResolverTypeWrapper<Scalars['Float']['output']>;
   Int: ResolverTypeWrapper<Scalars['Int']['output']>;
   Mutation: ResolverTypeWrapper<{}>;
+  Product: ResolverTypeWrapper<Product>;
+  ProductCreateInput: ProductCreateInput;
+  ProductStatus: ResolverTypeWrapper<ProductStatus>;
+  ProductUpdateInput: ProductUpdateInput;
+  Query: ResolverTypeWrapper<{}>;
+  Rental: ResolverTypeWrapper<Rental>;
+  RentalCreateInput: RentalCreateInput;
   String: ResolverTypeWrapper<Scalars['String']['output']>;
+  Transaction: ResolverTypeWrapper<Transaction>;
   User: ResolverTypeWrapper<User>;
-};
+}>;
 
 /** Mapping between all available schema types and the resolvers parents */
-export type ResolversParentTypes = {
-  AuthPayload: AuthPayload;
+export type ResolversParentTypes = ResolversObject<{
+  AuthPayload: Omit<AuthPayload, 'user'> & { user: ResolversParentTypes['User'] };
   Boolean: Scalars['Boolean']['output'];
+  Category: Category;
+  Float: Scalars['Float']['output'];
   Int: Scalars['Int']['output'];
   Mutation: {};
+  Product: Product;
+  ProductCreateInput: ProductCreateInput;
+  ProductUpdateInput: ProductUpdateInput;
+  Query: {};
+  Rental: Rental;
+  RentalCreateInput: RentalCreateInput;
   String: Scalars['String']['output'];
+  Transaction: Transaction;
   User: User;
-};
+}>;
 
-export type AuthPayloadResolvers<ContextType = any, ParentType extends ResolversParentTypes['AuthPayload'] = ResolversParentTypes['AuthPayload']> = {
+export type AuthPayloadResolvers<ContextType = any, ParentType extends ResolversParentTypes['AuthPayload'] = ResolversParentTypes['AuthPayload']> = ResolversObject<{
   token?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   user?: Resolver<ResolversTypes['User'], ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
-};
+}>;
 
-export type MutationResolvers<ContextType = any, ParentType extends ResolversParentTypes['Mutation'] = ResolversParentTypes['Mutation']> = {
+export type CategoryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Category'] = ResolversParentTypes['Category']> = ResolversObject<{
+  id?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  products?: Resolver<Maybe<Array<ResolversTypes['Product']>>, ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+}>;
+
+export type MutationResolvers<ContextType = any, ParentType extends ResolversParentTypes['Mutation'] = ResolversParentTypes['Mutation']> = ResolversObject<{
+  buyProduct?: Resolver<ResolversTypes['Transaction'], ParentType, ContextType, RequireFields<MutationBuyProductArgs, 'productId'>>;
+  createProduct?: Resolver<ResolversTypes['Product'], ParentType, ContextType, RequireFields<MutationCreateProductArgs, 'data'>>;
+  deleteProduct?: Resolver<ResolversTypes['Product'], ParentType, ContextType, RequireFields<MutationDeleteProductArgs, 'id'>>;
   login?: Resolver<ResolversTypes['AuthPayload'], ParentType, ContextType, RequireFields<MutationLoginArgs, 'email' | 'password'>>;
   register?: Resolver<ResolversTypes['AuthPayload'], ParentType, ContextType, RequireFields<MutationRegisterArgs, 'address' | 'email' | 'firstName' | 'lastName' | 'password'>>;
-};
+  rentProduct?: Resolver<ResolversTypes['Rental'], ParentType, ContextType, RequireFields<MutationRentProductArgs, 'data'>>;
+  updateProduct?: Resolver<ResolversTypes['Product'], ParentType, ContextType, RequireFields<MutationUpdateProductArgs, 'data' | 'id'>>;
+}>;
 
-export type UserResolvers<ContextType = any, ParentType extends ResolversParentTypes['User'] = ResolversParentTypes['User']> = {
+export type ProductResolvers<ContextType = any, ParentType extends ResolversParentTypes['Product'] = ResolversParentTypes['Product']> = ResolversObject<{
+  categories?: Resolver<Maybe<Array<Maybe<ResolversTypes['Category']>>>, ParentType, ContextType>;
+  description?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  owner?: Resolver<ResolversTypes['User'], ParentType, ContextType>;
+  ownerId?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  price?: Resolver<ResolversTypes['Float'], ParentType, ContextType>;
+  rentPeriod?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  rentPrice?: Resolver<Maybe<ResolversTypes['Float']>, ParentType, ContextType>;
+  rentals?: Resolver<Maybe<Array<ResolversTypes['Rental']>>, ParentType, ContextType>;
+  status?: Resolver<ResolversTypes['ProductStatus'], ParentType, ContextType>;
+  title?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  transactions?: Resolver<Maybe<Array<ResolversTypes['Transaction']>>, ParentType, ContextType>;
+  views?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+}>;
+
+export type ProductStatusResolvers = EnumResolverSignature<{ AVAILABLE?: any, RENTED?: any, SOLD?: any }, ResolversTypes['ProductStatus']>;
+
+export type QueryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = ResolversObject<{
+  categories?: Resolver<Array<ResolversTypes['Category']>, ParentType, ContextType>;
+  category?: Resolver<Maybe<ResolversTypes['Category']>, ParentType, ContextType, RequireFields<QueryCategoryArgs, 'id'>>;
+  myBorrowedProducts?: Resolver<Array<ResolversTypes['Rental']>, ParentType, ContextType>;
+  myBoughtProducts?: Resolver<Array<ResolversTypes['Transaction']>, ParentType, ContextType>;
+  myLentProducts?: Resolver<Array<ResolversTypes['Rental']>, ParentType, ContextType>;
+  myProduct?: Resolver<Maybe<ResolversTypes['Product']>, ParentType, ContextType, RequireFields<QueryMyProductArgs, 'id'>>;
+  myProducts?: Resolver<Array<ResolversTypes['Product']>, ParentType, ContextType>;
+  mySoldProducts?: Resolver<Array<ResolversTypes['Transaction']>, ParentType, ContextType>;
+  product?: Resolver<Maybe<ResolversTypes['Product']>, ParentType, ContextType, RequireFields<QueryProductArgs, 'id'>>;
+  products?: Resolver<Array<ResolversTypes['Product']>, ParentType, ContextType>;
+  rentals?: Resolver<Array<ResolversTypes['Rental']>, ParentType, ContextType>;
+  transactions?: Resolver<Array<ResolversTypes['Transaction']>, ParentType, ContextType>;
+  user?: Resolver<Maybe<ResolversTypes['User']>, ParentType, ContextType, RequireFields<QueryUserArgs, 'id'>>;
+  users?: Resolver<Array<ResolversTypes['User']>, ParentType, ContextType>;
+}>;
+
+export type RentalResolvers<ContextType = any, ParentType extends ResolversParentTypes['Rental'] = ResolversParentTypes['Rental']> = ResolversObject<{
+  createdAt?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  endDate?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  owner?: Resolver<ResolversTypes['User'], ParentType, ContextType>;
+  ownerId?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  product?: Resolver<ResolversTypes['Product'], ParentType, ContextType>;
+  productId?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  renter?: Resolver<ResolversTypes['User'], ParentType, ContextType>;
+  renterId?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  startDate?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+}>;
+
+export type TransactionResolvers<ContextType = any, ParentType extends ResolversParentTypes['Transaction'] = ResolversParentTypes['Transaction']> = ResolversObject<{
+  buyer?: Resolver<ResolversTypes['User'], ParentType, ContextType>;
+  buyerId?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  createdAt?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  product?: Resolver<ResolversTypes['Product'], ParentType, ContextType>;
+  productId?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  seller?: Resolver<ResolversTypes['User'], ParentType, ContextType>;
+  sellerId?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+}>;
+
+export type UserResolvers<ContextType = any, ParentType extends ResolversParentTypes['User'] = ResolversParentTypes['User']> = ResolversObject<{
   address?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  boughtProducts?: Resolver<Maybe<Array<ResolversTypes['Transaction']>>, ParentType, ContextType>;
   email?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   firstName?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   id?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
   lastName?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  lentProducts?: Resolver<Maybe<Array<ResolversTypes['Rental']>>, ParentType, ContextType>;
   password?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   phone?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  products?: Resolver<Maybe<Array<ResolversTypes['Product']>>, ParentType, ContextType>;
+  rentals?: Resolver<Maybe<Array<ResolversTypes['Rental']>>, ParentType, ContextType>;
+  rentedProducts?: Resolver<Maybe<Array<ResolversTypes['Rental']>>, ParentType, ContextType>;
+  soldProducts?: Resolver<Maybe<Array<ResolversTypes['Transaction']>>, ParentType, ContextType>;
+  transactions?: Resolver<Maybe<Array<ResolversTypes['Transaction']>>, ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
-};
+}>;
 
-export type Resolvers<ContextType = any> = {
+export type Resolvers<ContextType = any> = ResolversObject<{
   AuthPayload?: AuthPayloadResolvers<ContextType>;
+  Category?: CategoryResolvers<ContextType>;
   Mutation?: MutationResolvers<ContextType>;
+  Product?: ProductResolvers<ContextType>;
+  ProductStatus?: ProductStatusResolvers;
+  Query?: QueryResolvers<ContextType>;
+  Rental?: RentalResolvers<ContextType>;
+  Transaction?: TransactionResolvers<ContextType>;
   User?: UserResolvers<ContextType>;
-};
+}>;
 
